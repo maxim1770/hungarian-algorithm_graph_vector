e9e08a68a8d2cbaba64f6b694c7ef29775cbcc44
maxim1770 <maxim1770@gmail.com>
2022-06-15 09:33:16 +0300

    добавил пункт Б в виде комменатрия

--- hungarian-algorithm_graph_vector/hungarian-algorithm_graph_vector.cpp
+++ hungarian-algorithm_graph_vector/hungarian-algorithm_graph_vector.cpp
@@ -68,4 +68,72 @@ int main()
 	vector<pair<int, int>> result;
 	for (int j = 0; j < width; j++) if (markIndices[j] != -1) result.push_back(pair<int, int>(markIndices[j], j));
 	for (int i = 0; i < n; i++) cout << result[i].first + 1 << " -> " << result[i].second + 1 << endl;
+
+
+	//пункт Б
+	/*
+	#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <limits>
+using namespace std;
+
+const int inf = numeric_limits<int>::max();
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	srand(time(0));
+	cout << "Введите размер матрицы: ";
+	int n;
+	cin >> n;
+	vector<vector<int>> x(n);
+	for (int i = 0; i < n; i++) for (int j = 0; j < n; j++) x[i].push_back((rand() % 40) + 10);
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) cout << x[i][j] << " ";
+		cout << endl;
+	}
+
+	cout << endl;
+	int height = x.size(), width = x[0].size();
+	vector<int> u(height, 0), v(width, 0);
+	vector<int> markIndices(width, -1);
+	for (int i = 0; i < height; i++) {
+		vector<int> links(width, -1);
+		vector<int> maxs(width, inf);
+		vector<int> visited(width, 0);
+		int markedI = i, markedJ = -1, j;
+		while (markedI != -1) {
+			j = -1;
+			for (int j1 = 0; j1 < width; j1++)
+				if (!visited[j1]) {
+					if (x[markedI][j1] - u[markedI] - v[j1] < maxs[j1]) {
+						maxs[j1] = x[markedI][j1] - u[markedI] - v[j1];
+						links[j1] = markedJ;
+					}
+					if (j == -1 || maxs[j1] > maxs[j]) j = j1;
+				}
+			int delta = maxs[j];
+			for (int j1 = 0; j1 < width; j1++)
+				if (visited[j1]) {
+					u[markIndices[j1]] += delta;
+					v[j1] -= delta;
+				}
+				else maxs[j1] -= delta;
+			u[i] += delta;
+			visited[j] = 1;
+			markedJ = j;
+			markedI = markIndices[j];
+		}
+		for (; links[j] != -1; j = links[j]) markIndices[j] = markIndices[links[j]];
+		markIndices[j] = i;
+	}
+
+	vector<pair<int, int>> result;
+	for (int j = 0; j < width; j++) if (markIndices[j] != -1) result.push_back(pair<int, int>(markIndices[j], j));
+	for (int i = 0; i < n; i++) cout << result[i].first+1 << " -> " << result[i].second+1 << endl;
+}
+	*/
 }
